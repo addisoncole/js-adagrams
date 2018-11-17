b070b39f2d1294e71def1485b8697ad53fdd9a84
Addison Cole <addison.cole85@gmail.com>
2018-11-17 11:19:35 -0800

    create function scoreLetter, which takes in a single letter in a switch statement and returns the score of that letter. Create scoreWord function which takes a word and scores it based on value of letter and length of word.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,24 +1,63 @@
-const staticLetterHash = {
+const STATIC_LETTER_HASH = {
   A: 9, B: 2, C: 2, D: 4, E: 12,
   F: 2, G: 3, H: 2, I: 9, J: 1,
   K: 1, L: 4, M: 2, N: 6, O: 8,
   P: 2, Q: 1, R: 6, S: 4, T: 6,
   U: 4, V: 2, W: 2, X: 1, Y: 2,
   Z: 1
-}
+};
+
 const Adagrams = {
 
   createLetterPool(){
 
     let letterPool = []
 
-    for (let letter in staticLetterHash) {
-      let count = staticLetterHash[letter]
+    for (let letter in STATIC_LETTER_HASH) {
+      let count = STATIC_LETTER_HASH[letter]
       for (let i = 0; i < count; i += 1) {
         letterPool.push(letter);
       }
     }
     return letterPool
+  },
+
+  scoreLetter(letter){
+    switch(letter){
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+      case 'L':
+      case 'N':
+      case 'R':
+      case 'S':
+      case 'T':
+      return 1
+      case 'D':
+      case 'G':
+      return 2
+      case 'B':
+      case 'C':
+      case 'M':
+      case 'P':
+      return 3
+      case 'F':
+      case 'H':
+      case 'V':
+      case 'W':
+      case 'Y':
+      return 4
+      case 'K':
+      return 5
+      case 'J':
+      case 'X':
+      return 8
+      case 'Q':
+      case 'Z':
+      return 10
+    }
   },
 
   drawLetters() {
@@ -33,7 +72,7 @@ const Adagrams = {
       playerHand.push(letter);
     }
 
-    return playerHand;
+    return playerHand
   },
 
   //  let arr1 = ['w', 'o', 'r', 'd'];
@@ -48,17 +87,27 @@ const Adagrams = {
     checkLetters.sort();
 
     let i = 0;
-
     while (i < checkWord.length) {
-
       if (checkWord[i] === checkLetters[i]) {
         i++;
       } else {
-        return false;
+        return false
       }
     }
 
-    return true;
+    return true
+  },
+
+  scoreWord(word){
+    let score = 0;
+    let scoreWord = word.toUpperCase().split('');
+    if (scoreWord.length >= 7) {
+      score += 8;
+    }
+    scoreWord.forEach(function (letter){
+      score += Adagrams.scoreLetter(letter);
+    });
+    return score
   }
 
 };
